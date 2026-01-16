233927a428a8450e6a388909c6d8d2c1ce4ab6cf
Dimitrije Stanković <diki.zajecar@gmail.com>
2026-01-16 19:25:28 +0100

    musko zensko

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -198,7 +198,6 @@ export default function UserProfile() {
               <option value="">Select Gender</option>
               <option value="male">Male</option>
               <option value="female">Female</option>
-              <option value="other">Other</option>
             </select>
           </div>
 
